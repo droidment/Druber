31c92f934738b053e2322293a7be26bc6a9b1050
rganta@mindagile.com <rganta@mindagile.com>
2015-02-28 18:51:52 -0800

    added logic to include user object
    added logic to include user object

--- Druber/cloud/main.js
+++ Druber/cloud/main.js
@@ -8,14 +8,44 @@ Parse.Cloud.define("hello", function(request, response) {
 
 // added comment - ravi
 Parse.Cloud.define("getAllTimeSlots", function(request,response) {
-              var query = new Parse.Query(TimeSlot);
+              var query = new Parse.Query("TimeSlot");
+			  query.include("doctorPointer");
+			  
 			query.find({
 			  success: function(results) {
 				// results is an array of Parse.Object.
+				response.success(results);
 			  },
 
 			  error: function(error) {
 				// error is an instance of Parse.Error.
+				response.error(error);
+			  }
+        });
+});
+
+Parse.Cloud.define("getValidTimeSlotsNearPatient", function(request,response) {
+			var lat = request.params.lat;
+			var lon = request.params.lon;
+			var range = request.params.range;
+			var timeRange =  request.params.timeRange; // 1d, 2d, 1h, 2h, etc.
+			
+            var query = new Parse.Query("TimeSlot");
+			query.include("doctorPointer");
+			 
+			var point = new Parse.GeoPoint(lat, lon);			
+			query.select("objectId","location","displayAddress", "displayName");
+			query.near("location", point);
+			
+			query.find({
+			  success: function(results) {
+				// results is an array of Parse.Object.
+				response.success(results);
+			  },
+
+			  error: function(error) {
+				// error is an instance of Parse.Error.
+				response.error(error);
 			  }
         });
 });
